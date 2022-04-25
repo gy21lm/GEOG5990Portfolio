50752394193b3a301550582632647da9cb54bfce
gy21lm <lenarmoule@gmail.com>
2022-04-25 17:57:37 +0100

    Shrink code for model.py
    Use for-loops to shrink code.

--- model.py
+++ model.py
@@ -3,65 +3,44 @@ import random
 import operator 
 import matplotlib.pyplot
 
-# Set up agent list. 
+# Set up agents.
+num_of_agents = 10 
 agents = []
 
-# Set up random coordinates (y0 and x0) to be placed in list. 
-agents.append([random.randint(0,99),random.randint(0,99)])
+# Make the agents (random y and x values). 
+for i in range(num_of_agents):
+    agents.append([random.randint(0,99),random.randint(0,99)])
 
-# Move y0 one step.
-if random.random() < 0.5:
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
+print(agents)
+
+# Move the agents.
+for i in range(num_of_agents):
     
-print(agents[0][0])
-
-# Move x0 one step.
-if random.random() < 0.5:
-    agents[0][1] += 1 
-else:
-    agents[0][1] -= 1
-
-print(agents[0][1])
+    #Move one step in y direction.
+    if random.random() < 0.5:
+        agents[i][0] += 1
+    else:
+        agents[i][0] -= 1
+    
+    # Move one step in x direction.
+    if random.random() < 0.5:
+        agents[i][1] += 1 
+    else:
+        agents[i][1] -= 1
 
 print(agents)
 
 
-
-# Set up second set of random coordinates (y1 and x1) to be placed in list. 
-agents.append([random.randint(0,99),random.randint(0,99)])
-
-# Move y1 one step.
-if random.random() < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-    
-print(agents[1][0])
-
-# Move x1 one step. 
-if random.random() < 0.5:
-    agents[1][1] += 1 
-else:
-    agents[1][1] -= 1
-
-print(agents[1][1])
-
-print(agents)
-
-
-
-# Work out pythagorian distance between two points. 
+'''# Work out pythagorian distance between two points. 
 answer = (((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2))**0.5
 
 #Print answer.
-print(answer)
+print(answer)'''
 
-# Print max y value.
+# Print agent with the max y value.
 print(max(agents))
 
-# Print max x value.
+# Print agent with the max x value.
 print(max(agents, key=operator.itemgetter(1)))
 
 # Matplotlib 
@@ -69,6 +48,8 @@ matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
 matplotlib.pyplot.scatter(agents[0][1],agents[0][0])
 matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
+# Colour the most easterly agent red. 
 m = max(agents, key=operator.itemgetter(1))
 matplotlib.pyplot.scatter(m[1],m[0], color='red')
+# Plot data. 
 matplotlib.pyplot.show()

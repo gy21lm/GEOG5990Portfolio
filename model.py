31ab70f306699d53994afa4ba4dae74f46c8eb16
gy21lm <lenarmoule@gmail.com>
2022-04-26 00:26:52 +0100

    Update model.py
    Now possible to move the agents as many times as desired (as defined by num_of_iterations), as well as fixing boundary effects by using a torus.

--- model.py
+++ model.py
@@ -4,7 +4,8 @@ import operator
 import matplotlib.pyplot
 
 # Set up agents.
-num_of_agents = 10 
+num_of_agents = 10
+num_of_iterations = 100 
 agents = []
 
 # Make the agents (random y and x values). 
@@ -16,26 +17,29 @@ print(agents)
 # Move the agents.
 for i in range(num_of_agents):
     
-    #Move one step in y direction.
-    if random.random() < 0.5:
-        agents[i][0] += 1
-    else:
-        agents[i][0] -= 1
+    for j in range(num_of_iterations):
     
-    # Move one step in x direction.
-    if random.random() < 0.5:
-        agents[i][1] += 1 
-    else:
-        agents[i][1] -= 1
+        #Move one step in y direction.
+        if random.random() < 0.5:
+            agents[i][0] = (agents[i][0] + 1) % 100
+        else:
+            agents[i][0] = (agents[i][0] - 1) % 100
+        
+        # Move one step in x direction.
+        if random.random() < 0.5:
+            agents[i][1] = (agents[i][1] + 1) % 100
+        else:
+            agents[i][1] = (agents[i][1] - 1) % 100
 
 print(agents)
 
-
-'''# Work out pythagorian distance between two points. 
+'''
+# Work out pythagorian distance between two points. 
 answer = (((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2))**0.5
 
 #Print answer.
-print(answer)'''
+print(answer)
+'''
 
 # Print agent with the max y value.
 print(max(agents))
@@ -43,13 +47,13 @@ print(max(agents))
 # Print agent with the max x value.
 print(max(agents, key=operator.itemgetter(1)))
 
-# Matplotlib 
+# Set axes limits and plot agents. 
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.scatter(agents[0][1],agents[0][0])
-matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
+for i in range(num_of_agents):
+    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
 # Colour the most easterly agent red. 
 m = max(agents, key=operator.itemgetter(1))
 matplotlib.pyplot.scatter(m[1],m[0], color='red')
-# Plot data. 
+# Display all open figures. 
 matplotlib.pyplot.show()

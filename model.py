a1e9e637ed7a5722c2044d7d09def55d2b08a844
gy21lm <lenarmoule@gmail.com>
2022-04-26 20:10:33 +0100

    Update model.py
    Removed code that initialises and moves agents and has been put into the Agent class in agentframework.py

--- model.py
+++ model.py
@@ -1,9 +1,7 @@
 # Import modules.
-import random
-import operator 
 import matplotlib.pyplot
+import agentframework
 
-# Calculate distance between a pair of agents (fucntion declaration)
 def distance_between(agents_row_a, agents_row_b):
     '''Calculate the pythagorian distance between a pair of agents.
     
@@ -20,64 +18,33 @@ def distance_between(agents_row_a, agents_row_b):
         Pythagorian distance between first coordinate and second coordinate. 
 
     '''
-    return (((agents_row_a[0] - agents_row_b[0])**2) 
-            + ((agents_row_a[1] - agents_row_b[1])**2))**0.5
-
+    return (((agents_row_a.y - agents_row_b.y)**2) 
+            + ((agents_row_a.x - agents_row_b.x)**2))**0.5
 
 # Set up agents.
 num_of_agents = 10
 num_of_iterations = 100 
 agents = []
 
-# Make the agents (random y and x values). 
+# Make the agents. 
 for i in range(num_of_agents):
-    agents.append([random.randint(0,99),random.randint(0,99)])
-
-print(agents)
+    agents.append(agentframework.Agent())
 
 # Move the agents.
 for j in range(num_of_iterations):
 
     for i in range(num_of_agents):
        
-        #Move one step in y direction.
-        if random.random() < 0.5:
-            agents[i][0] = (agents[i][0] + 1) % 100
-        else:
-            agents[i][0] = (agents[i][0] - 1) % 100
-        
-        # Move one step in x direction.
-        if random.random() < 0.5:
-            agents[i][1] = (agents[i][1] + 1) % 100
-        else:
-            agents[i][1] = (agents[i][1] - 1) % 100
-
-print(agents)
-
-# Print agent with the max y value.
-print(max(agents))
-
-# Print agent with the max x value.
-print(max(agents, key=operator.itemgetter(1)))
+        agents[i].move()
 
 # Set axes limits and plot agents. 
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
 for i in range(num_of_agents):
-    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+    matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
 matplotlib.pyplot.show()
 
 #Calculate distance between all agents 
-distance = []
 for agents_row_a in agents:
     for agents_row_b in agents:
-        # Optimising search
-        if agents_row_a > agents_row_b: 
-            distance.append(distance_between(agents_row_a, agents_row_b))
-            
-# Min and max distance between agents 
-print(max(distance))
-print(min(distance))
-
-# Check the search has been optimised correctly.
-print(len(distance))
+        distance = distance_between(agents_row_a, agents_row_b)

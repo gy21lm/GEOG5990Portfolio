c819402c5c4d38a403f541c6884b2bd2d8372556
gy21lm <lenarmoule@gmail.com>
2022-04-26 02:37:17 +0100

    Update model.py (functions)
    Adding a function to calculate the distance between all agents.

--- model.py
+++ model.py
@@ -3,6 +3,27 @@ import random
 import operator 
 import matplotlib.pyplot
 
+# Calculate distance between a pair of agents (fucntion declaration)
+def distance_between(agents_row_a, agents_row_b):
+    '''Calculate the pythagorian distance between a pair of agents.
+    
+    Parameters
+    ----------
+    agents_row_a : two integers in a list
+        Integer coordinates. 
+    agents_row_b : two integers in a list
+        Second integer coordinates.
+
+    Returns
+    -------
+    float
+        Pythagorian distance between first coordinate and second coordinate. 
+
+    '''
+    return (((agents_row_a[0] - agents_row_b[0])**2) 
+            + ((agents_row_a[1] - agents_row_b[1])**2))**0.5
+
+
 # Set up agents.
 num_of_agents = 10
 num_of_iterations = 100 
@@ -15,10 +36,10 @@ for i in range(num_of_agents):
 print(agents)
 
 # Move the agents.
-for i in range(num_of_agents):
-    
-    for j in range(num_of_iterations):
-    
+for j in range(num_of_iterations):
+
+    for i in range(num_of_agents):
+       
         #Move one step in y direction.
         if random.random() < 0.5:
             agents[i][0] = (agents[i][0] + 1) % 100
@@ -33,14 +54,6 @@ for i in range(num_of_agents):
 
 print(agents)
 
-'''
-# Work out pythagorian distance between two points. 
-answer = (((agents[0][0] - agents[1][0])**2) + ((agents[0][1] - agents[1][1])**2))**0.5
-
-#Print answer.
-print(answer)
-'''
-
 # Print agent with the max y value.
 print(max(agents))
 
@@ -52,8 +65,19 @@ matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
-# Colour the most easterly agent red. 
-m = max(agents, key=operator.itemgetter(1))
-matplotlib.pyplot.scatter(m[1],m[0], color='red')
-# Display all open figures. 
 matplotlib.pyplot.show()
+
+#Calculate distance between all agents 
+distance = []
+for agents_row_a in agents:
+    for agents_row_b in agents:
+        # Optimising search
+        if agents_row_a > agents_row_b: 
+            distance.append(distance_between(agents_row_a, agents_row_b))
+            
+# Min and max distance between agents 
+print(max(distance))
+print(min(distance))
+
+# Check the search has been optimised correctly.
+print(len(distance))

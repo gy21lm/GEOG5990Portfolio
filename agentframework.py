433484723d885d4f6965edeea0ac69e033a6bb3d
gy21lm <lenarmoule@gmail.com>
2022-04-27 08:19:18 +0100

    Adding functions to agentframwork.py

--- agentframework.py
+++ agentframework.py
@@ -9,9 +9,12 @@ Created on Tue Apr 26 17:56:22 2022
 import random
 
 class Agent():
-    def __init__(self):
+    def __init__(self, environment, agents):
         self._y = random.randint(0,99)
         self._x = random.randint(0,99)
+        self.environment = environment 
+        self.store = 0
+        self.agents = agents
         
     def gety(self):
         return self._y
@@ -42,3 +45,19 @@ class Agent():
         else:
             self.x = (self.x - 1) % 100
             
+    def eat(self):
+        if self.environment[self.y][self.x] > 10:
+            self.environment[self.y][self.x] -= 10
+            self.store += 10 
+            
+    def share_with_neighbours(self, neighbourhood):
+        for agent in self.agents:
+            dist = self.distance_between(agent)
+            if dist <= neighbourhood:
+                sum = self.store + agent.store
+                ave = sum /2
+                self.store = ave
+                agent.store = ave
+            
+    def distance_between(self, agent):
+        return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5 

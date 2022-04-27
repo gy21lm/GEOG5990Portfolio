b963a4cb5bc201c1cde72308cc47f9165af8d704
gy21lm <lenarmoule@gmail.com>
2022-04-27 15:33:17 +0100

    Add animation output to model.py
    Add animation output to model.py that stops when all agents have a higher store value than that of the stopping condition.

--- model.py
+++ model.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 09:21:44 2022
+
+@author: Lena
+"""
+
 # Import modules.
 import random
 import matplotlib.pyplot
+import matplotlib.animation
 import agentframework
 import csv
 
@@ -17,23 +25,64 @@ with open('in.txt', newline='') as f:
 # Set up model parameters. 
 num_of_agents = 10
 num_of_iterations = 100
+stopping_condition = 25000
 neighbourhood = 20 
 
 # Set up agents list. 
 agents = []
 
+# Set up figure size 
+fig = matplotlib.pyplot.figure(figsize=(7, 7))
+ax = fig.add_axes([0, 0, 1, 1])
+
 # Make the agents. 
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent(environment, agents))
+    agents.append(agentframework.Agent(environment, rowlist, agents))
+    
+carry_on = True
 
-# Move the agents.
-for j in range(num_of_iterations):
-    random.shuffle(agents)
+def update(frame_number):
+    
+    fig.clear()
+    global carry_on
+
+    # Move the agents.
+    for j in range(num_of_iterations):
+        random.shuffle(agents)
+        for i in range(num_of_agents):
+            agents[i].move()
+            agents[i].eat()
+            agents[i].share_with_neighbours(neighbourhood)
+    
+    store = []
     for i in range(num_of_agents):
-        agents[i].move()
-        agents[i].eat()
-        agents[i].share_with_neighbours(neighbourhood)
+        store.append(agents[i].store)
+    print(store)
+ 
+    for i in range(len(store)):
+        if all(store[i] > stopping_condition for store[i] in store):
+            carry_on = False
+    
+    # Set axes limits and plot agents.
+    matplotlib.pyplot.ylim(0, len(environment))
+    matplotlib.pyplot.xlim(0, len(rowlist))
+    matplotlib.pyplot.imshow(environment)       
+    
+    for i in range(num_of_agents):
+        matplotlib.pyplot.scatter(agents[i].x,agents[i].y, color='white')
+        
+def gen_function(b = [0]):
+    a = 0
+    global carry_on #Not actually needed as we're not assigning, but clearer
+    while (carry_on) :
+        yield a			# Returns control and waits next call.
+        a = a + 1
+        
+animation = matplotlib.animation.FuncAnimation(fig, update, interval = 1, repeat=False, frames=gen_function)
 
+matplotlib.pyplot.show()
+
+'''
 # Set axes limits and plot agents. 
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
@@ -41,3 +90,4 @@ matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
 matplotlib.pyplot.show()
+'''

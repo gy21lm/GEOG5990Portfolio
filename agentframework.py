8639f852f8057718b19d6da094099586b511cc02
gy21lm <lenarmoule@gmail.com>
2022-04-28 02:34:49 +0100

    Update agentframework.py
    Update move function to run a different amount of times depending on new speed variable.
    Add comments to code.

--- agentframework.py
+++ agentframework.py
@@ -4,12 +4,29 @@ Created on Tue Apr 26 17:56:22 2022
 
 @author: Lena
 """
-
-
 import random
 
 class Agent():
+    """Provides methods for agents. 
+    
+    move -- agent moves in the y and x direction and 'eats' from new 
+        environment its positioned on 
+    eat -- agent 'eats' the environment it's positioned on
+    share_with_neighbours -- agent shares its store with another agent if the 
+        second agent is within its neighbourhood
+    """
     def __init__(self, environment, rowlist, agents, y, x):
+        """Initialises the object's attritbutes when an object is created from
+        a class.
+
+        Parameters
+        ----------
+        environment -- list
+        rowlist -- list
+        agents -- list
+        y -- number
+        x -- number 
+        """
         self.environment = environment
         self.rowlist = rowlist
         self.store = 0
@@ -41,25 +58,54 @@ class Agent():
         del self._x
     x = property(getx, setx, delx, "I'm the 'x' property.")
     
-    def move(self):
-        #Move one step in y direction.
-        if random.random() < 0.5:
-            self.y = (self.y + 1) % len(self.environment)
-        else:
-            self.y = (self.y - 1) % len(self.environment)
+    def move(self, speed):
+        """Moves the agent one step in the y and x direction, as well as 
+        running the eat function as many times as determined by 'speed'.
         
-        # Move one step in x direction.
-        if random.random() < 0.5:
-            self.x = (self.x + 1) % len(self.rowlist)
-        else:
-            self.x = (self.x - 1) % len(self.rowlist)
+        Parameters
+        ----------
+        speed -- integer
+        """            
+        for i in range(speed):    
+            #Move one step in y direction.
+            if random.random() < 0.5:
+                self.y = (self.y + 1) % len(self.environment)
+                self.eat()
+
+            else:
+                self.y = (self.y - 1) % len(self.environment)
+                self.eat()
+            
+            # Move one step in x direction.
+            if random.random() < 0.5:
+                self.x = (self.x + 1) % len(self.rowlist)
+                self.eat()
+
+            else:
+                self.x = (self.x - 1) % len(self.rowlist)
+                self.eat()
             
     def eat(self):
+        """Agent 'eats' 10 units, storing them within itself, and removing 
+        them from the environment where the agent is positioned IF the 
+        environment has a value larger than 10 at the position of the agent.
+        """
         if self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
             self.store += 10 
             
     def share_with_neighbours(self, neighbourhood):
+        """Agent shares its store if another agent is located within its
+        neighbourhood.
+        
+        Store is shared by taking the sum of both agents' stores and 
+        allocating each agent half of the sum. 
+
+        Parameters
+        ----------
+        neighbourhood -- number 
+            The distance from the agent defining the neighbourhood
+        """
         for agent in self.agents:
             dist = self.distance_between(agent)
             if dist <= neighbourhood:
@@ -69,4 +115,14 @@ class Agent():
                 agent.store = ave
             
     def distance_between(self, agent):
+        """Calculates the pythagorian distance between two agents. 
+
+        Parameters
+        ----------
+        agent -- a list containing coordinate values 
+
+        Returns
+        -------
+        floating-point number 
+        """
         return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5 
